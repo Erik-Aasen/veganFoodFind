4db2f50503db0bcfb1c119c8671c00d0872c9e89
Erik <erik@Eriks-MacBook-Pro-2.local>
2021-08-07 21:17:52 -0700

    Modify static hosting directory in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -326,10 +326,10 @@ app.put("/addmeal", async (req: Request, res: Response) => {
   }
 })
 
-app.use(express.static(path.join(__dirname, "../../client/build/")));
+app.use(express.static(path.join(__dirname, "../../../client/build/")));
 
 app.get("/*", (req, res) => {
- res.sendFile(path.join(__dirname, "../../client/build/", "index.html"));
+ res.sendFile(path.join(__dirname, "../../../client/build/", "index.html"));
 });
 
 app.listen(process.env.PORT || 4000, () => {
