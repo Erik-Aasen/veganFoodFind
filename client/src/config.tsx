811ccfe979d1aab09fdc369cc9f2ff5ff2a6183f
Erik <erik@Eriks-MacBook-Pro-2.local>
2021-08-10 00:25:03 -0700

    hopefully fix config

--- client/src/config.tsx
+++ client/src/config.tsx
@@ -1,11 +1,11 @@
 // var API="https://vegan-food-find.herokuapp.com";
-var API = 'http://localhost:4000'
+// var API = 'http://localhost:4000'
 
 if (process.env.NODE_ENV === "development") {
     // eslint-disable-next-line
     var API = "http://localhost:4000"
 
-} else if (process.env.NODE_ENV === "production") {
+} else {
     // eslint-disable-next-line
     var API = "https://vegan-food-find.herokuapp.com"
     // const API = "/"
