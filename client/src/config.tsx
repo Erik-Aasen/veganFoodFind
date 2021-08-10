de520fbdc81dc1c8173e82924fa58255db3c9cca
Erik <erik@Eriks-MacBook-Pro-2.local>
2021-08-10 10:21:03 -0700

    Clean up config.tsx

--- client/src/config.tsx
+++ client/src/config.tsx
@@ -1,13 +1,10 @@
-// var API="https://vegan-food-find.herokuapp.com";
-// var API = 'http://localhost:4000'
-
 if (process.env.NODE_ENV === "development") {
     // eslint-disable-next-line
     var API = "http://localhost:4000"
 
 } else {
-    // eslint-disable-next-line
     // var API = "https://vegan-food-find.herokuapp.com"
+    // eslint-disable-next-line
     var API = ""
 }
 
