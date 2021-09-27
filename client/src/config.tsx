338033009d1d21108b151c2f76857bf9391b4115
Erik <erik@Eriks-MacBook-Pro-2.local>
2021-09-26 20:22:47 -0700

    Delete commented line in config.tsx

--- client/src/config.tsx
+++ client/src/config.tsx
@@ -6,16 +6,15 @@ if (process.env.NODE_ENV === "development") {
 if (process.env.NODE_ENV === 'production') {
     // eslint-disable-next-line
     var API = "https://vegan-food-find.herokuapp.com"
-    // var API = ""
 }
 
-    // eslint-disable-next-line
 if (process.env.NODE_ENV === 'test') {
     // eslint-disable-next-line
     var API = "https://vegan-food-find-staging.herokuapp.com"
 }
 
 else {
+    // eslint-disable-next-line
     var API = '';
 }
 
