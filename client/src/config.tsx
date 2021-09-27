1b2c3ed8f078a0729576199bcc24467b6050ec64
Erik <erik@Eriks-MacBook-Pro-2.local>
2021-09-26 19:45:46 -0700

    Add staging NODE_ENV

--- client/src/config.tsx
+++ client/src/config.tsx
@@ -1,11 +1,19 @@
 if (process.env.NODE_ENV === "development") {
     // eslint-disable-next-line
     var API = "http://localhost:4000"
+} 
 
-} else {
+if (process.env.NODE_ENV === 'production') {
     // eslint-disable-next-line
     var API = "https://vegan-food-find.herokuapp.com"
     // var API = ""
+}
+
+else
+//  (process.env.NODE_ENV === 'staging') 
+ {
+    // eslint-disable-next-line
+    var API = "https://vegan-food-find-staging.herokuapp.com"
 }
 
 export default API;
