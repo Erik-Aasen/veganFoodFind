7da84d36b3a6caf1bf40ac84b3f4c300f5fc46db
Erik <erik@Eriks-MacBook-Pro-2.local>
2021-09-26 20:08:59 -0700

    Change API definition in client config, update mongoose and remove mongoose type package

--- client/src/config.tsx
+++ client/src/config.tsx
@@ -1,22 +1,15 @@
+let API;
+
 if (process.env.NODE_ENV === "development") {
-    // eslint-disable-next-line
-    var API = "http://localhost:4000"
+    API = "http://localhost:4000"
 } 
 
 if (process.env.NODE_ENV === 'production') {
-    // eslint-disable-next-line
-    var API = "https://vegan-food-find.herokuapp.com"
-    // var API = ""
+    API = "https://vegan-food-find.herokuapp.com"
 }
 
-    // eslint-disable-next-line
 if (process.env.NODE_ENV === 'test') {
-    // eslint-disable-next-line
-    var API = "https://vegan-food-find-staging.herokuapp.com"
-}
-
-else {
-    var API = '';
+    API = "https://vegan-food-find-staging.herokuapp.com"
 }
 
 export default API;
