786b82d32e4d9c5597f1a111971768a38d1a6f02
Erik <erik@Eriks-MacBook-Pro-2.local>
2021-10-02 21:26:59 -0700

    Change config to work for local, staging, and production

--- client/src/config.tsx
+++ client/src/config.tsx
@@ -1,22 +1,21 @@
-if (process.env.NODE_ENV === "development") {
-    // eslint-disable-next-line
-    console.log(true);
-    
-    var API = "http://localhost:4000"
-} 
-
 if (process.env.NODE_ENV === 'production') {
     // eslint-disable-next-line
     var API = "https://vegan-food-find.herokuapp.com"
 }
 
-else
-//  (process.env.NODE_ENV === 'test') 
- {
+if (process.env.NODE_ENV === 'test') {
     // eslint-disable-next-line
     var API = "https://vegan-food-find-staging.herokuapp.com"
 }
 
+if (process.env.NODE_ENV === 'development') {
+    // eslint-disable-next-line
+    var API = "http://localhost:4000"
+}
 
+else {
+    // eslint-disable-next-line
+    var API = ''
+}
 
 export default API;
