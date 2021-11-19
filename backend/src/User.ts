996a77fb0325a32930b9bdd518fef9edaf1480c7
Erik <erik@Eriks-MacBook-Pro-2.local>
2021-11-19 00:20:41 -0800

    Outline steps to implement email confirmation

--- backend/src/User.ts
+++ backend/src/User.ts
@@ -37,3 +37,22 @@ const user = new mongoose.Schema({
 });
 
 export default mongoose.model<MongoInterface>("User", user);
+
+// Create new categories in user Schema:
+    // isVerified default false
+    // confirmation code
+    // confirmation code date
+
+// When user registers:
+    // create confirmation code: jwt from email and jwtSecret (in env)
+    // store confirmation code in db
+    // send email with api/{email}/{confirmation code}
+
+// When user clicks email link:
+    // handle by api/:email/:confirmation code
+    // if email exists, check if that user has that confirmation code within the expiration date
+        // if so, mark user as verified
+        // and delete confirmation code from db
+
+
+
