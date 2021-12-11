cacd0bba169142b88d241d2a17daf8054e4f1c75
Erik <erik@Eriks-MacBook-Pro-2.local>
2021-12-11 00:03:33 -0800

    Add button to resetpassword

--- client/src/Pages/ResetPassword.tsx
+++ client/src/Pages/ResetPassword.tsx
@@ -1,25 +1,32 @@
 import { useState } from "react"
- import { Form, Button } from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap'
 
- export default function ResetPassword() {
+export default function ResetPassword() {
+
+    const [usernameEmail, setUsernameEmail] = useState<string>('')
+
+    const changePassword = async (e) => {
+        console.log(usernameEmail);
+    }
+
+    return (
+        <div className='login'>
+            <h1>Reset Password</h1>
+            <p className='verification-fail text-primary'>Enter your username or email to receive a link in your inbox to reset your password.</p>
+            <Form>
+                <Form.Group>
+                    <Form.Control
+                        type='text'
+                        placeholder='Username or email'
+                        onChange={e => setUsernameEmail(e.target.value)}
+                    />
+                </Form.Group>
+            </Form>
+            <br />
+            <Button onClick={e => { changePassword(e) }}>Submit</Button>
+        </div>
+    )
 
 
 
-     return (
-         <div className='login'>
-             <h1>Reset Password</h1>
-             <p className='verification-fail text-primary'>Enter your username or email to receive a link in your inbox to reset your password.</p>
-             <Form>
-                 <Form.Group>
-                     <Form.Control
-                         type='text'
-                         placeholder='Username or email'
-                     />
-                 </Form.Group>
-             </Form>
-         </div>
-     )
-
-
-
- } 
+}
